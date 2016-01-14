79406189938f515e8bfb06d86868200c18b32d09
nickstokes <nick.stokes@gmail.com>
2016-01-14 15:48:13 +1000

    increased indentation to 8

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,15 @@
 ## Write a short comment describing this function
 ## creates a special matrix that also holds the inverse of itself.
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(xinv) inv <<- xinv
-  getinv <- function() inv
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(xinv) inv <<- xinv
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("cached inverse found, returning")
-    return(inv)
-  }
-  mtrx <- x$get()
-  inv <- solve(mtrx)
-  x$setinv(inv)
-  inv
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("cached inverse found, returning")
+                return(inv)
+        }
+        mtrx <- x$get()
+        inv <- solve(mtrx)
+        x$setinv(inv)
+        inv
 }
